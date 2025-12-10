e1ac281a16c11f0de5c31eaf1f89698725e79607
DanielPetrowski <petrowskidaniel@web.de>
2025-12-10 13:59:50 +0100

    fixed header initials menü

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,18 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  standalone:true,
-  imports: [CommonModule,RouterLinkActive,RouterLink],
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './header.html',
-  styleUrl: './header.scss',
+  styleUrls: ['./header.scss']
 })
 export class Header {
- menuOpen = false;
+  menuOpen = false;
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+  this.menuOpen = false;
+}
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+
+    // Wenn Menü offen ist
+    if (this.menuOpen) {
+      const clickedInsideMenu = target.closest('.menu');
+      const clickedOnMenuIcon = target.closest('.menu-icon');
+
+      if (!clickedInsideMenu && !clickedOnMenuIcon) {
+        this.menuOpen = false;
+      }
+    }
   }
 }
