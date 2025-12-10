1d08cd90c58032a749be839a3595fd203c22c53d
DanielPetrowski <petrowskidaniel@web.de>
2025-12-10 13:30:03 +0100

    add initials menü

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule,RouterLinkActive,RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
+ menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
