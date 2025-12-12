ffadc41523426879b1ad0958ba30db8bece6da65
Rohric <emilmarsal@icloud.com>
2025-12-12 10:56:28 +0100

    try fix dialog backgrop close

--- src/app/shared/dialog/dialog.ts
+++ src/app/shared/dialog/dialog.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, Input, Output, } from '@angular/core';
 
 @Component({
   selector: 'app-dialog',
@@ -14,21 +14,45 @@ export class Dialog {
   isOpen = false;
   isClosing = false;
 
-  open(): void {
-    this.isClosing = false;
-    this.isOpen = true;
-  }
+  // open(): void {
+  //   this.isClosing = false;
+  //   this.isOpen = true;
+  // }
 
-  close(): void {
+  // close(): void {
+  //   if (this.isClosing) return;
+
+  //   this.isClosing = true;    
+
+  //   setTimeout(() => {  
+  //     this.isClosing = false;
+  //     this.isOpen = false;
+  //     this.closed.emit();
+  //   }, 240);
+  // }
+
+
+  /* ---------------------------- Fix Versuch Emil ---------------------------- */
+  constructor(private cdr: ChangeDetectorRef) {}
+  
+
+    close(): void {
     if (this.isClosing) return;
+    this.isClosing = true;
+    this.cdr.markForCheck();
 
-    this.isClosing = true;    
-
-    setTimeout(() => {  
+    setTimeout(() => {
       this.isClosing = false;
       this.isOpen = false;
       this.closed.emit();
-    }, 240);
+      this.cdr.markForCheck(); // CDR kommt von anular
+    }, 200); // zeit passt nicht ganz, der übergng zu background sieht komisch aus
+  }
+
+  open(): void {
+    this.isClosing = false;
+    this.isOpen = true;
+    this.cdr.markForCheck();
   }
 }
 
