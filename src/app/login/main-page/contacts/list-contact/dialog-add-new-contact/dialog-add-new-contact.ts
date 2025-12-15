cd00737832762c38e97aaf39b1700ac1e6d7a3ad
Rohric <emilmarsal@icloud.com>
2025-12-15 17:37:50 +0100

    add new dialog components

---
+++ src/app/login/main-page/contacts/list-contact/dialog-add-new-contact/dialog-add-new-contact.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-add-new-contact',
+  imports: [],
+  templateUrl: './dialog-add-new-contact.html',
+  styleUrl: './dialog-add-new-contact.scss',
+})
+export class DialogAddNewContact {
+
+}
---
+++ src/app/login/main-page/contacts/single-contact/dialog-edit-contact/dialog-edit-contact.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-edit-contact',
+  imports: [],
+  templateUrl: './dialog-edit-contact.html',
+  styleUrl: './dialog-edit-contact.scss',
+})
+export class DialogEditContact {
+
+}
